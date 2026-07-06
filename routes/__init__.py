018e0afaa9705f91a10c0e141c4d6810dcae2146
superboygisan <superboygisan@gmail.com>
2026-07-06 19:08:04 +0530

    Update __init__.py

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,1 +1,16 @@
-/* Placeholder for routes/__init__.py */
+# routes/__init__.py
+# Routes blueprint initialization
+
+from routes.auth import auth_bp
+from routes.tracks import tracks_bp
+from routes.albums import albums_bp
+from routes.admin import admin_bp
+from routes.rooms import rooms_bp
+
+__all__ = [
+    'auth_bp',
+    'tracks_bp', 
+    'albums_bp',
+    'admin_bp',
+    'rooms_bp'
+]
